f01945bbf4b43c0073c8888ca59fbdbaacfc944c
Keith <32609074+wkeithchau@users.noreply.github.com>
2024-12-02 08:26:57 +1030

    Start working on day01

---
+++ .day01-historian-hysteria/index.js
@@ -0,0 +1,14 @@
+import { getInput } from '../utils/input.js'
+import { parseLists } from './utils.js'
+
+const INPUT = getInput(import.meta.url, '\n')
+const DATA = parseLists(INPUT)
+
+console.log(DATA)
+
+const part1 = () => {}
+
+const part2 = () => {}
+
+part1()
+part2()
