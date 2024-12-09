a87df05807b08aef36752b9a30725d54c5679fd9
Keith Chau <wkeith.chau@gmail.com>
2024-12-09 22:04:45 +1030

    Complete day04 part 2

--- day04-ceres-search/index.js
+++ day04-ceres-search/index.js
@@ -1,8 +1,7 @@
 import { getInput } from '../utils/input.js'
-import { searchWord } from './search.js'
+import { crossSearchWord, searchWord } from './search.js'
 
 const INPUT = getInput(import.meta.url, '\n')
-// const DATA = INPUT.map((data) => String(data))
 
 const part1 = () => {
   const word = 'XMAS'
@@ -11,7 +10,12 @@ const part1 = () => {
   console.log(`The word '${word}' appears: ${count} time(s)`)
 }
 
-const part2 = () => {}
+const part2 = () => {
+  const word = 'MAS'
+  const count = crossSearchWord(word, INPUT)
+
+  console.log(`The word '${word}' appears in a cross pattern: ${count} time(s)`)
+}
 
 part1()
 part2()
